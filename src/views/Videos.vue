44711e9b143ff3a94fac4bf8c8cacd61ca76ad0a
Palanka777 <dpont@iesebre.com>
2022-01-25 17:26:59 +0100

    Acabo el video 105

--- src/views/Videos.vue
+++ src/views/Videos.vue
@@ -1,24 +1,125 @@
 <template>
   <ion-page>
-    <ion-list>
-      <ion-list-header> Videos </ion-list-header>
+    <ion-content>
+      <ion-refresher slot="fixed" @ion-Refresh="refresh" id="refresher">
+        <ion-refresher-content></ion-refresher-content>
+      </ion-refresher>
+      <ion-list id="skeleton" v-if="loading">
+        <ion-list-header>
+          <ion-skeleton-text animated style="width: 80px"></ion-skeleton-text>
+        </ion-list-header>
+        <ion-item>
+          <ion-avatar slot="start">
+            <ion-skeleton-text></ion-skeleton-text>
+          </ion-avatar>
+          <ion-label>
+            <h3>
+              <ion-skeleton-text animated style="width: 80%"></ion-skeleton-text>
+            </h3>
+            <p>
+              <ion-skeleton-text animated style="width: 60%"></ion-skeleton-text>
+            </p>
+          </ion-label>
+        </ion-item>
+        <ion-item>
+          <ion-avatar slot="start">
+            <ion-skeleton-text></ion-skeleton-text>
+          </ion-avatar>
+          <ion-label>
+            <h3>
+              <ion-skeleton-text animated style="width: 80%"></ion-skeleton-text>
+            </h3>
+            <p>
+              <ion-skeleton-text animated style="width: 60%"></ion-skeleton-text>
+            </p>
 
-      <ion-item v-for="video in videos" :key="video.id">
-        <ion-avatar slot="start">
-          <img src="http://via.placeholder.com/80" />
-        </ion-avatar>
-        <ion-label>
-          <h2>{{ video.title }}</h2>
-          <p>{{ video.description}}</p>
-        </ion-label>
-      </ion-item>
+          </ion-label>
+        </ion-item>
+        <ion-item>
+          <ion-avatar slot="start">
+            <ion-skeleton-text></ion-skeleton-text>
+          </ion-avatar>
+          <ion-label>
+            <h3>
+              <ion-skeleton-text animated style="width: 80%"></ion-skeleton-text>
+            </h3>
+            <p>
+              <ion-skeleton-text animated style="width: 60%"></ion-skeleton-text>
+            </p>
 
-    </ion-list>
+          </ion-label>
+        </ion-item>
+        <ion-item>
+          <ion-avatar slot="start">
+            <ion-skeleton-text></ion-skeleton-text>
+          </ion-avatar>
+          <ion-label>
+            <h3>
+              <ion-skeleton-text animated style="width: 80%"></ion-skeleton-text>
+            </h3>
+            <p>
+              <ion-skeleton-text animated style="width: 60%"></ion-skeleton-text>
+            </p>
+          </ion-label>
+        </ion-item>
+        <ion-item>
+          <ion-avatar slot="start">
+            <ion-skeleton-text></ion-skeleton-text>
+          </ion-avatar>
+          <ion-label>
+            <h3>
+              <ion-skeleton-text animated style="width: 80%"></ion-skeleton-text>
+            </h3>
+            <p>
+              <ion-skeleton-text animated style="width: 60%"></ion-skeleton-text>
+            </p>
+          </ion-label>
+        </ion-item>
+        <ion-item>
+          <ion-avatar slot="start">
+            <ion-skeleton-text></ion-skeleton-text>
+          </ion-avatar>
+          <ion-label>
+            <h3>
+              <ion-skeleton-text animated style="width: 80%"></ion-skeleton-text>
+            </h3>
+            <p>
+              <ion-skeleton-text animated style="width: 60%"></ion-skeleton-text>
+            </p>
+
+          </ion-label>
+        </ion-item>
+
+      </ion-list>
+      <ion-list v-else>
+        <ion-list-header> Videos </ion-list-header>
+
+        <ion-item v-for="video in videos" :key="video.id">
+          <ion-avatar slot="start">
+            <img src="http://via.placeholder.com/80" />
+          </ion-avatar>
+          <ion-label>
+            <h2>{{ video.title }}</h2>
+            <p>{{ video.description}}</p>
+          </ion-label>
+        </ion-item>
+
+      </ion-list>
+    </ion-content>
   </ion-page>
 </template>
 
 <script>
-import {IonAvatar, IonItem, IonLabel, IonList, IonListHeader, IonPage} from "@ionic/vue";
+import {
+  IonAvatar, IonContent,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonListHeader,
+  IonPage,
+  IonRefresher,
+  IonRefresherContent, IonSkeletonText
+} from "@ionic/vue";
 import casteaching from "@acacha/casteaching";
 
 export default {
@@ -30,14 +131,33 @@ export default {
     IonAvatar,
     IonLabel,
     IonPage,
+    IonRefresher,
+    IonRefresherContent,
+    IonContent,
+    IonSkeletonText
+
   },
   data() {
     return {
-      videos:{}
+      videos:[],
+      loading:true
     }
   },
   async created() {
-    this.videos = await casteaching.videos()
+    await this.fetchVideos()
+    this.loading=false
+  },
+  mounted() {
+    this.refresher = document.getElementById('refresher');
+  },
+  methods:{
+    async refresh() {
+      await this.fetchVideos()
+      this.refresher.complete();
+    },
+    async fetchVideos(){
+      this.videos = await casteaching.videos()
+    }
   }
 }
 </script>
